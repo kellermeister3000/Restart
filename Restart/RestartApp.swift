bddf350c7120c273301095a16fb4884c9bf1c64f
Philip Keller <philipjoelkeller@gmail.com>
2023-03-09 14:01:24 -0800

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Philip Keller on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
